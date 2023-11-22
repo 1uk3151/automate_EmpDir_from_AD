9a9e354346e3bc8507980c385d2ef48f3c1fc534
1uk3151 <120432365+1uk3151@users.noreply.github.com>
2023-11-22 11:45:21 -0600

    Add files via upload
    Simplified script. JSON output is no longer saved as a file.

--- automateEmployeeDir.py
+++ automateEmployeeDir.py
@@ -5,39 +5,27 @@ from getpass import getpass
 
 # Active Directory Server parameters (hostname, username, password, powershell script location)
 
-AD_SVR_HOSTNAME = "DC01"
+AD_SVR_HOSTNAME = "TESTSERVER"
     # Hostname of the server running Active Directory (Domain Controller)
 
-AD_SVR_USERNAME = "luke.mosley"
+AD_SVR_USERNAME = "liason"
     # User account on domain controller that has privileges to run Get-ADUser script
 
 AD_SVR_PASSWORD = getpass()
     # Password of user. You will be asked for your password in the terminal after running script.
 
-PWSH_SCRIPT = "C:\\it_files\\automateEmployeeDir\\AD_UserInfo_To_JSON.ps1"
-    # Location of Get-ADUser script. This script should direct the output to a JSON formatted file. See file below:
-        # param(
-        #     [string]$OutFile
-        # )
-        #
-        # Get-ADUser -Filter 'UserPrincipalName -notlike "null"' -Properties * |
-        #   select Surname, GivenName, OfficePhone, Office, Department, UserPrincipalName, Title |
-        #   ConvertTo-Json | Out-File $OutFile
-
-JSON_FILE = "z:\\automateEmployeeDir\\employeedir.json"
-    # Location of JSON output from the PWSH_SCRIPT
-
-HTML_OUTPUT = "z:\\automateEmployeeDir\\employee.html"
-    # Location of HTML output
+HTML_OUTPUT = "/var/www/testsite/employee.html"
+    # Location of HTML output on web server
 
 
 # STEP 1: Run Get_ADUser script on domain controller which creates JSON file.
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(hostname=AD_SVR_HOSTNAME, username=AD_SVR_USERNAME, password=AD_SVR_PASSWORD)
-stdin, stdout, stderr = client.exec_command(f"{PWSH_SCRIPT} -OutFile {JSON_FILE}")
+stdin, stdout, stderr = client.exec_command(f"""Get-ADUser -Filter 'UserPrincipalName -notlike "null"' -Properties * | 
+select Surname, GivenName, OfficePhone, Office, Department, UserPrincipalName, Title | 
+ConvertTo-Json""")
 stdin.close()
-print(stdout.read().decode())
 print(stderr.read().decode())
 client.close()
 
@@ -45,9 +33,7 @@ client.close()
 # STEP 2: Pull user info from JSON file and place in HTML template.
 htmltemplate = HTMLTemplate(html_file=HTML_OUTPUT)
 
-
-with open(JSON_FILE, encoding="utf16") as json_file:
-    json_data = json.load(json_file)
+json_data = json.load(stdout)
 
 htmltemplate.heading()
 
@@ -62,15 +48,4 @@ for i in range(len(json_data)):
         phone=employee["OfficePhone"],
         email=employee["UserPrincipalName"]
     )
-
-
-# ubietest = paramiko.SSHClient()
-# ubietest.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-# ubietest.connect(hostname="ubietest", username="liason", password="Cisco123!")
-# stdin, stdout, stderr = ubietest.exec_command("cp /home/luke/sharedfolder/automateEmployeeDir/employee.html /var/www/testsite")
-# stdin.close()
-# print(stdout.read().decode())
-# print(stderr.read().decode())
-# ubietest.close()
-
 
