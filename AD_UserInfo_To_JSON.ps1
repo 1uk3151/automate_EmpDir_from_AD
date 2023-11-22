2d9a30eed29c1b44858226ba75636cab48d89abd
1uk3151 <120432365+1uk3151@users.noreply.github.com>
2023-11-22 13:56:33 -0600

    Delete AD_UserInfo_To_JSON.ps1
    PowerShell script not needed. The powershell command is written directly into the Python script

--- AD_UserInfo_To_JSON.ps1
+++
@@ -1,5 +0,0 @@
-﻿param(
-    [string]$OutFile
-)
-
-Get-ADUser -Filter 'UserPrincipalName -notlike "null"' -Properties * | select Surname, GivenName, OfficePhone, Office, Department, UserPrincipalName, Title | ConvertTo-Json | Out-File $OutFile
